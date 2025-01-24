5f2bfa53ddc4626ff3540b570068e1d94eeaf7f2
buongiornodev <159156318+Buongiorno-DEV@users.noreply.github.com>
2025-01-24 18:52:47 +0100

    Update sv_moto.lua

--- sv_moto.lua
+++ sv_moto.lua
@@ -1,8 +1,1 @@
-print("Script /moto sviluppato da BuongiornoDEV, entra nel discord se vuoi altri script GRATIS: https://discord.gg/SMGJU4PqGJ")
 
-RegisterCommand("moto", function(source, args, rawCommand)
-    local playerId = source
-    local playerPed = GetPlayerPed(playerId)
-    local coordinate = GetEntityCoords(playerPed)
-    TriggerClientEvent("spawnMoto", playerId, coordinate)
-end, false)
